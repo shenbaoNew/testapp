0092048e73fe4c3fed0a92ace1a9a78801b088c2
shenbao <shenbao@digiwin.com>
2022-03-17 22:20:13 +0800

    测试应用外网能否直接访问

--- src/main/java/com/digiwin/testapp/service/dap/RestTest.java
+++ src/main/java/com/digiwin/testapp/service/dap/RestTest.java
@@ -20,7 +20,10 @@ import java.util.Map;
  * Description:
  */
 public class RestTest {
-    public void test() {
+    public static void main(String[] args) {
+        test2();
+    }
+    public static void test() {
         RestTemplate restTemplate = new RestTemplate();
         String json = "{\n" +
                 "  \"std_data\": {\n" +
@@ -44,9 +47,40 @@ public class RestTest {
         httpHeaders.add("digi-key", "6f63960bf7ea10aed76414138274339d");
         httpHeaders.add("digi-host", "{\"prod\":\"Athena\",\"ver\":\"1.0\",\"ip\":\"\",\"id\":\"AgileInteraction\",\"timestamp\":\"20220220133016673\",\"lang\":\"zh_TW\",\"eoc_acct\":\"athena\",\"acct\":\"athena\"}");
         httpHeaders.add("digi-eocmap", "{\"eoc_company_id\":\"1\",\"eoc_region_id\":\"\",\"eoc_site_id\":\"1\"}");
-        httpHeaders.add("token", "40a61160-ad67-4336-8240-56aea08b6e97");
+        httpHeaders.add("token", "c618575b-8713-4169-8c1a-2697b40683fb");
         HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, httpHeaders);
         ResponseEntity<Map> mapResponseEntity = restTemplate.postForEntity("https://esp-test.apps.digiwincloud.com.cn/CROSS/RESTful",
+                entity, Map.class);
+        System.out.println(mapResponseEntity);
+    }
+
+    public static void test2() {
+        RestTemplate restTemplate = new RestTemplate();
+        String json = "{\n" +
+                "  \"std_data\": {\n" +
+                "    \"parameter\": {\n" +
+                "      \"query_condition\": [\n" +
+                "        {\n" +
+                "          \"responsible_person_no\": \"pur004\",\n" +
+                "          \"executor_no\": \"pur001\",\n" +
+                "          \"data_status\": \"0\"\n" +
+                "        }\n" +
+                "      ],\n" +
+                "      \"enterprise_no\": \"1\",\n" +
+                "      \"site_no\": \"1\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        Map<String, Object> body = new Gson().fromJson(json, Map.class);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("digi-type", "sync");
+        httpHeaders.add("digi-service", "{\"prod\":\"MKP\",\"tenant_id\":\"E10ATHENA\",\"name\":\"supplier.delivery.plan.daily.info.get\",\"uid\":\"MKP\"}");
+        httpHeaders.add("digi-key", "6f63960bf7ea10aed76414138274339d");
+        httpHeaders.add("digi-host", "{\"prod\":\"Athena\",\"ver\":\"1.0\",\"ip\":\"\",\"id\":\"AgileInteraction\",\"timestamp\":\"20220220133016673\",\"lang\":\"zh_TW\",\"eoc_acct\":\"athena\",\"acct\":\"athena\"}");
+        httpHeaders.add("digi-eocmap", "{\"eoc_company_id\":\"1\",\"eoc_region_id\":\"\",\"eoc_site_id\":\"1\"}");
+        httpHeaders.add("token", "c618575b-8713-4169-8c1a-2697b40683fb");
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<Map> mapResponseEntity = restTemplate.postForEntity("https://mkp-test.apps.digiwincloud.com.cn/eai",
                 entity, Map.class);
         System.out.println(mapResponseEntity);
     }
