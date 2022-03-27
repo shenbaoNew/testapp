3243caafd1db6dfbaaa86854fce027cd979d3181
shenbao <shenbao@digiwin.com>
2022-03-27 23:22:23 +0800

    扫描.class

--- src/main/java/com/digiwin/testapp/design/OtherDemo.java
+++ src/main/java/com/digiwin/testapp/design/OtherDemo.java
@@ -1,8 +1,12 @@
 package com.digiwin.testapp.design;
 
+import com.digiwin.testapp.TestappApplication;
 import com.digiwin.testapp.design.prototype.ProtoTypeCache;
 import com.digiwin.testapp.design.prototype.ProtoTypeObject;
 import com.digiwin.testapp.design.single.SingleObject5;
+
+import java.io.File;
+import java.net.URL;
 
 /**
  * FileName:OtherDemo
@@ -11,5 +15,13 @@ import com.digiwin.testapp.design.single.SingleObject5;
  * Description:
  */
 public class OtherDemo {
-
+    public static void main(String[] args) {
+        String packageName = "com.digiwin.testapp.design.proxy";
+        URL resource = TestappApplication.class.getClassLoader().getResource(packageName.replace(".", "/"));
+        System.out.println(resource);
+        File file = new File(resource.getFile());
+        System.out.println(file.isDirectory());
+        for (int i = 0; i < file.listFiles().length; i++) {
+        }
+    }
 }
